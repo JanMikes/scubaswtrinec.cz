a6b0b961a53e4750e7fd9dfcba3127d44e221627
Jan Mikeš <j.mikes@me.com>
2014-01-06 15:33:34 +0100

    Added JS console tweak

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -1,10 +1,33 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+	var method;
+	var noop = function () {};
+	var methods = [
+		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+		'timeStamp', 'trace', 'warn'
+	];
+	var length = methods.length;
+	var console = (window.console = window.console || {});
+
+	while (length--) {
+		method = methods[length];
+
+		// Only stub undefined methods.
+		if (!console[method]) {
+			console[method] = noop;
+		}
+	}
+}());
+
 $(function(){
 
 	$.nette.init();
 
 		$.ajaxSetup({
-	    cache: false,
-	    dataType: 'json'
+		cache: false,
+		dataType: 'json'
 	});
 
 	$('<div id="ajax-spinner" style="position: fixed;left: 50%;top: 50%;display: none;">Loading<br /><i class="fa fa-spinner fa-spin fa-3x"></i></div>').appendTo("body");
