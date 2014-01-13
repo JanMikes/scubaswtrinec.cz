b9f4fe4d07b3fcd0954ad2e73fd3b0d75860bc50
Jan Mikeš <j.mikes@me.com>
2014-01-13 13:11:02 +0100

    Updated install script to work on ubuntu server

--- install.sh
+++ install.sh
@@ -2,36 +2,38 @@
 clear
 
 # Clearing web scripts temp folder and/or creating it
-rm -rf ./www/webtemp/ &> /dev/null && mkdir ./www/webtemp/ &> /dev/null && echo "$(tput setaf 2)Scripts temp directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating www/webtemp directory with appropriate access!$(tput sgr0)"
-chmod 777 ./www/webtemp/ &> /dev/null || echo "$(tput setaf 1)Failed setting write rights to www/webtemp!$(tput sgr0)"
+rm -rf ./www/webtemp/
+mkdir ./www/webtemp/ && echo "$(tput setaf 2)Scripts temp directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating www/webtemp directory with appropriate access!$(tput sgr0)"
+chmod 777 ./www/webtemp/ || echo "$(tput setaf 1)Failed setting write rights to www/webtemp!$(tput sgr0)"
 
 # Clearing temp folder and/or creating it
-rm -rf ./temp/ &> /dev/null && mkdir -p ./temp/sessions/ &> /dev/null && echo "$(tput setaf 2)Temp directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating temp directory with appropriate access!$(tput sgr0)"
-chmod -R 777 ./temp/ &> /dev/null || echo "$(tput setaf 1)Failed setting write rights to temp!$(tput sgr0)"
+rm -rf ./temp/
+mkdir -p ./temp/sessions/ && echo "$(tput setaf 2)Temp directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating temp directory with appropriate access!$(tput sgr0)"
+chmod -R 777 ./temp/ || echo "$(tput setaf 1)Failed setting write rights to temp!$(tput sgr0)"
 
 # Copying system files into temp directory
-cp ./libs/.htaccess ./temp/.htaccess &> /dev/null || echo "$(tput setaf 1)Failed creating temp/.htaccess!$(tput sgr0)"
-cp ./libs/web.config ./temp/web.config &> /dev/null || echo "$(tput setaf 1)Failed creating temp/web.config!$(tput sgr0)"
+cp ./libs/.htaccess ./temp/.htaccess || echo "$(tput setaf 1)Failed creating temp/.htaccess!$(tput sgr0)"
+cp ./libs/web.config ./temp/web.config || echo "$(tput setaf 1)Failed creating temp/web.config!$(tput sgr0)"
 
 # Creates log directory, if does not exist
 if [ ! -e ./log/ ]
 then
-    mkdir -p ./log/ &> /dev/null && echo "$(tput setaf 2)Log directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating log directory!$(tput sgr0)"
-    chmod 777 ./log/ &> /dev/null || echo "$(tput setaf 1)Failed setting write rights to log!$(tput sgr0)"
+    mkdir -p ./log/ && echo "$(tput setaf 2)Log directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating log directory!$(tput sgr0)"
+    chmod 777 ./log/ || echo "$(tput setaf 1)Failed setting write rights to log!$(tput sgr0)"
 fi
 
 # Copying system files into log directory
-cp ./libs/.htaccess ./log/.htaccess &> /dev/null || echo "$(tput setaf 1)Failed creating log/.htaccess!$(tput sgr0)"
+cp ./libs/.htaccess ./log/.htaccess || echo "$(tput setaf 1)Failed creating log/.htaccess!$(tput sgr0)"
 cp ./libs/web.config ./log/web.config || echo "$(tput setaf 1)Failed creating log/web.config!$(tput sgr0)"
 
 # Creates packagist directory, if does not exist
 if [ ! -e ./packagist/ ]
 then
-    mkdir -p ./packagist/ &> /dev/null && echo "$(tput setaf 2)Packagist directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating packagist directory!$(tput sgr0)"
+    mkdir -p ./packagist/ && echo "$(tput setaf 2)Packagist directory created successfully.$(tput sgr0)" || echo "$(tput setaf 1)Failed creating packagist directory!$(tput sgr0)"
 fi
 
 # Copying system files into packagist directory
-cp ./libs/.htaccess ./packagist/.htaccess &> /dev/null || echo "$(tput setaf 1)Failed creating packagist/.htaccess!$(tput sgr0)"
+cp ./libs/.htaccess ./packagist/.htaccess || echo "$(tput setaf 1)Failed creating packagist/.htaccess!$(tput sgr0)"
 cp ./libs/web.config ./packagist/web.config || echo "$(tput setaf 1)Failed creating packagist/web.config!$(tput sgr0)"
 
 # Creating local config file from template, if does not exist
@@ -44,7 +46,7 @@ fi
 
 # Installing composer from composer.lock file
 # TODO: Download composer.phar from remote and delete it after installing
-composer install &> /dev/null && echo "$(tput setaf 2)Composer successfully installed.$(tput sgr0)" || echo "Failed installing composer!"
+composer install
 
 # Thannk you!
 echo "" ; echo "Thank you for using janmikes.cz Sandbox!" ; echo ""
