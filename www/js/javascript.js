9102472e004d960b00e12739043e800991d8e47f
Jan Mikeš <j.mikes@me.com>
2014-01-18 17:32:24 +0100

    Google analytics update

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -46,15 +46,17 @@ $(function(){
 	});
 
 	// Nette ajax google analytics extension
-	(function ($, _gaq) {
-		$.nette.ext('ga', {
-			success: function (payload) {
-				var url = payload.url || payload.redirect;
-				if (url && !$.nette.ext('redirect')) {
-					_gaq.push(['_trackPageview', url]);
+	if (typeof _gaq != 'undefined') {
+		(function ($, _gaq) {
+			$.nette.ext('ga', {
+				success: function (payload) {
+					var url = payload.url || payload.redirect;
+					if (url && !$.nette.ext('redirect')) {
+						_gaq.push(['_trackPageview', url]);
+					}
 				}
-			}
-		});
-	})(jQuery, _gaq);
+			});
+		})(jQuery, _gaq);
+	}
 
 });
