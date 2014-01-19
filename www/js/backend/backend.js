6d080cff067e8d9db34a71f5ae794705e4cbcbb7
Jan Mikeš <j.mikes@me.com>
2014-01-19 17:49:50 +0100

    Scripts tweaks + added live form validation for backend

--- www/js/backend/backend.js
+++ www/js/backend/backend.js
@@ -1,3 +1,28 @@
 $(function(){
 
+	$("input[maxlength], textarea[maxlength]").each(function(){
+		$(this).after("<span class=\"maxlength-info\"></span>");
+		maxlengthInfo($(this));
+	}).on("change blur keydown keypress keyup", function() {
+		maxlengthInfo($(this));
+	});
+
 });
+
+function maxlengthInfo ($el) {
+	var max = $el.attr("maxlength");
+	var act = $el.val().length;
+
+	var $span = $el.parent().find(".maxlength-info");
+
+	$span.html("<span>Zbývá " + (max-act) + " znaků z " + max + "</span>");
+
+	if (act >= max) {
+		$span.addClass("form-warning-message");
+		$span.removeClass("form-success-message");
+	} else {
+		$span.removeClass("form-warning-message");
+		$span.addClass("form-success-message");
+	}
+
+}
