3268fe53989a2de23381bc1d28458394c1d1c565
Jan Mikeš <j.mikes@me.com>
2014-02-28 00:49:09 +0100

    Added inject enabler

---
+++ libs/InjectEnabler/Extension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\InjectEnabler;
+
+use Nette\Configurator,
+	Nette\DI\Compiler,
+	Nette\DI\CompilerExtension;
+
+if (!class_exists('Nette\DI\CompilerExtension')) {
+	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
+	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
+	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
+}
+
+/**
+ *  @author Jan Mikes <j.mikes@me.com>
+ *  @copyright Jan Mikes - janmikes.cz
+ */
+final class Extension extends CompilerExtension
+{
+	const EXTENSION_NAME = "injectEnabler";
+
+
+	public function beforeCompile()
+	{
+		$builder = $this->getContainerBuilder();
+		$definitions = $builder->getDefinitions();
+
+		foreach ($builder->definitions as $definition) {
+			if ($definition->implement && method_exists($definition->implement, 'create')) {
+				$definition->setInject(TRUE);
+			}
+		}
+	}
+
+
+	public function install(Configurator $configurator)
+	{
+		$self = $this;
+		$configurator->onCompile[] = function ($configurator, Compiler $compiler) use ($self) {
+			$compiler->addExtension($self::EXTENSION_NAME, $self);
+		};
+	}
+}
